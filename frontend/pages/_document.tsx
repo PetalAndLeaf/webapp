d9a41d30dcff1c17e66cda718127dfcb77400675
Jian Ruan <jianruan@rjmbp.local>
2019-10-05 02:23:57 -0700

    fixed stylesheet problem when materialUI and styled-component co-exit

--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -6,31 +6,61 @@ import Document, {
   NextScript
 } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
+import { ServerStyleSheets } from '@material-ui/styles'
 import theme from '../styles/theme'
 
 class MyDocument extends Document {
+  // static async getInitialProps(ctx: DocumentContext) {
+  //   const sheet = new ServerStyleSheet()
+  //   const originalRenderPage = ctx.renderPage
+
+  //   try {
+  //     ctx.renderPage = () =>
+  //       originalRenderPage({
+  //         enhanceApp: App => props => sheet.collectStyles(<App {...props} />)
+  //       })
+
+  //     const initialProps = await Document.getInitialProps(ctx)
+  //     return {
+  //       ...initialProps,
+  //       styles: (
+  //         <>
+  //           {initialProps.styles}
+  //           {sheet.getStyleElement()}
+  //         </>
+  //       )
+  //     }
+  //   } finally {
+  //     sheet.seal()
+  //   }
+  // }
+
   static async getInitialProps(ctx: DocumentContext) {
-    const sheet = new ServerStyleSheet()
+    const styledComponentsSheet = new ServerStyleSheet()
+    const materialSheets = new ServerStyleSheets()
     const originalRenderPage = ctx.renderPage
 
     try {
       ctx.renderPage = () =>
         originalRenderPage({
-          enhanceApp: App => props => sheet.collectStyles(<App {...props} />)
+          enhanceApp: App => props =>
+            styledComponentsSheet.collectStyles(
+              materialSheets.collect(<App {...props} />)
+            )
         })
-
       const initialProps = await Document.getInitialProps(ctx)
       return {
         ...initialProps,
         styles: (
-          <>
+          <React.Fragment>
             {initialProps.styles}
-            {sheet.getStyleElement()}
-          </>
+            {materialSheets.getStyleElement()}
+            {styledComponentsSheet.getStyleElement()}
+          </React.Fragment>
         )
       }
     } finally {
-      sheet.seal()
+      styledComponentsSheet.seal()
     }
   }
 
