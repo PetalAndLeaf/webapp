43023a0a1bdff0d54a8ab60c83746bfd6f55df1d
Jian Ruan <jianruan@rjmbp.local>
2019-10-05 02:08:13 -0700

    mitigate problem of content jump on first load, _document

--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -1,23 +1,53 @@
 import React from 'react'
-import Document, { Head, Main, NextScript } from 'next/document'
-import { ServerStyleSheets } from '@material-ui/styles'
+import Document, {
+  DocumentContext,
+  Head,
+  Main,
+  NextScript
+} from 'next/document'
+import { ServerStyleSheet } from 'styled-components'
 import theme from '../styles/theme'
 
 class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const sheet = new ServerStyleSheet()
+    const originalRenderPage = ctx.renderPage
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: App => props => sheet.collectStyles(<App {...props} />)
+        })
+
+      const initialProps = await Document.getInitialProps(ctx)
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        )
+      }
+    } finally {
+      sheet.seal()
+    }
+  }
+
   render() {
     return (
-      <html lang="en">
+      <html lang='en'>
         <Head>
-          <meta charSet="utf-8" />
+          <meta charSet='utf-8' />
           <meta
-            name="viewport"
-            content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no"
+            name='viewport'
+            content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no'
           />
           {/* PWA primary color */}
-          <meta name="theme-color" content={theme.palette.primary.main} />
+          <meta name='theme-color' content={theme.palette.primary.main} />
           <link
-            href="https://fonts.googleapis.com/css?family=Lato:400,700|Quicksand:400,600&display=swap"
-            rel="stylesheet"
+            href='https://fonts.googleapis.com/css?family=Lato:400,700|Quicksand:400,600&display=swap'
+            rel='stylesheet'
           ></link>
         </Head>
         <body>
@@ -29,7 +59,7 @@ class MyDocument extends Document {
           }
            html {
             scroll-behavior: smooth;
-          
+
            }
            a {
              text-decoration: none;
@@ -41,50 +71,26 @@ class MyDocument extends Document {
   }
 }
 
-MyDocument.getInitialProps = async ctx => {
-  // Resolution order
-  //
-  // On the server:
-  // 1. app.getInitialProps
-  // 2. page.getInitialProps
-  // 3. document.getInitialProps
-  // 4. app.render
-  // 5. page.render
-  // 6. document.render
-  //
-  // On the server with error:
-  // 1. document.getInitialProps
-  // 2. app.render
-  // 3. page.render
-  // 4. document.render
-  //
-  // On the client
-  // 1. app.getInitialProps
-  // 2. page.getInitialProps
-  // 3. app.render
-  // 4. page.render
-
-  // Render app and page and get the context of the page with collected side effects.
-  const sheets = new ServerStyleSheets()
-  const originalRenderPage = ctx.renderPage
-
-  ctx.renderPage = () =>
-    originalRenderPage({
-      enhanceApp: App => props => sheets.collect(<App {...props} />)
-    })
-
-  const initialProps = await Document.getInitialProps(ctx)
-
-  return {
-    ...initialProps,
-    // Styles fragment is rendered after the app and page rendering finish.
-    styles: [
-      <React.Fragment key="styles">
-        {initialProps.styles}
-        {sheets.getStyleElement()}
-      </React.Fragment>
-    ]
-  }
-}
-
 export default MyDocument
+
+// Resolution order
+//
+// On the server:
+// 1. app.getInitialProps
+// 2. page.getInitialProps
+// 3. document.getInitialProps
+// 4. app.render
+// 5. page.render
+// 6. document.render
+//
+// On the server with error:
+// 1. document.getInitialProps
+// 2. app.render
+// 3. page.render
+// 4. document.render
+//
+// On the client
+// 1. app.getInitialProps
+// 2. page.getInitialProps
+// 3. app.render
+// 4. page.render
