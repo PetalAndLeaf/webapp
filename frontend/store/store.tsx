dc63b3c00a2ed485a65b7bc8223c8d53f4f8bab1
Jian Ruan <jianruan@rjmbp.local>
2019-10-13 20:16:53 -0700

    add sysn btw redux store and localstorage

--- frontend/store/store.tsx
+++ frontend/store/store.tsx
@@ -1,11 +1,13 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import thunkMiddleware from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
+import { loadState } from './localStorage'
 
 import { sessionReducer } from './session/reducers'
 import { userReducer } from './user/reducers'
 import { contentReducer } from './content/reducers'
 import { cartReducer } from './cart/reducer'
+import { isEmpty } from 'lodash'
 
 const rootReducer = combineReducers({
   session: sessionReducer,
@@ -19,6 +21,12 @@ export type AppState = ReturnType<typeof rootReducer>
 export function initStore(initialState: any) {
   const middlewares = [thunkMiddleware]
   const middleWareEnhancer = applyMiddleware(...middlewares)
+
+  //load state from local Storage if exists
+  const persistedState = loadState()
+  if (!isEmpty(persistedState)) {
+    initialState = persistedState.state
+  }
 
   const store = createStore(
     rootReducer,
