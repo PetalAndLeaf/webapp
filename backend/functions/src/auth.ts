b617c6025fcd235c1f40dc8db25df178e9c0506e
Jian Ruan <jianruan@rjmbp.local>
2019-10-14 22:39:21 -0700

    backend custom claim format and comment

--- backend/functions/src/auth.ts
+++ backend/functions/src/auth.ts
@@ -2,6 +2,8 @@ import * as functions from 'firebase-functions'
 import { auth } from './firebase'
 import { CallableContext } from 'firebase-functions/lib/providers/https'
 
+//client needs to have functions installed
+//then const addAdminRose_func = functions.httpsCallable('addAdminRose')
 export const addAdminRole = functions.https.onCall(
   async (data: any, context: CallableContext) => {
     const user = await auth.getUserByEmail(data.email)
