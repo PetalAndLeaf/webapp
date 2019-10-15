3dd4a1dd03ede8fa98eeb09b64c9a4cd28746f2d
Amy Pu <amypu@amypu-macbookpro2.roam.corp.google.com>
2019-10-14 23:48:50 -0700

    added account pages

---
+++ frontend/pages/account/payment.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { setConfig, setFooter } from '../../store/content/action'
+import { Typography } from '@material-ui/core'
+import AccountLayout from '../../layout/AccountLayout'
+import styled from 'styled-components'
+
+const Header = styled.div`
+  height: 64px;
+  min-height: 64px;
+  display: flex;
+  align-items: center;
+  margin-bottom: 16px;
+`
+export default function Payment() {
+  return (
+    <AccountLayout>
+      <Header>
+        <Typography variant="h4">Your Payment info</Typography>
+      </Header>
+    </AccountLayout>
+  )
+}
+
+Payment.getInitialProps = async function(ctx: any) {
+  const { store, isServer } = ctx
+  // store.dispatch(closeSidebar())
+  if (isServer) {
+    await store.dispatch(setConfig())
+    await store.dispatch(setFooter())
+  }
+  return {
+    isServer
+  }
+}
